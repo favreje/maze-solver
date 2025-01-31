c77ef5670b73a6c9b1c098b3f1aaa77442e89c08
favreje <favreje@gmail.com>
2025-01-31 16:48:16 -0600

    Added Point and Line classes

--- main.py
+++ main.py
@@ -6,11 +6,22 @@
 # --------------------------------------------------------------------------------------------------
 
 
-from window import Window
+from window import Window, Line, Point
 
 
 def main():
     win = Window(800, 600)
+
+    # Testing line drawing ------------------------------------------------------------------------
+    lines = (
+        Line(Point(10, 10), Point(300, 300)),
+        Line(Point(300, 300), Point(300, 1000)),
+        Line(Point(300, 1000), Point(800, 1000)),
+    )
+    for line in lines:
+        win.draw_line(line)
+    # ----------------------------------------------------------------------------------------------
+
     win.wait_for_close()
 
 
--- window.py
+++ window.py
@@ -14,6 +14,9 @@ class Window:
         self.__root.update_idletasks()
         self.__root.update()
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
     def wait_for_close(self):
         self.__is_running = True
         while self.__is_running:
@@ -22,3 +25,42 @@ class Window:
 
     def close(self):
         self.__is_running = False
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
+
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        return f"Start of line: {self.p1}  End of line: {self.p2}"
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x,
+            self.p1.y,
+            self.p2.x,
+            self.p2.y,
+            fill=fill_color,
+            width=2,
+        )
+
+
+def main():
+    p1 = Point(42, 69)
+    p2 = Point(42, 10)
+    l = Line(p1, p2)
+    print(l)
+
+
+if __name__ == "__main__":
+    main()
